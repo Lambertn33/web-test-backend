ed37233e4f7266ae5b7c6b36755237786db9fb89
Lambertn33 <lambertgithub@gmail.com>
2023-05-09 18:16:09 +0200

    feat: add .env

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,12 @@ import express, { Express } from "express";
 import bodyParser from "body-parser";
 import loginRoute from "./routes/auth.route";
 import cors  from "cors";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const main = () => {
   const app: Express = express();
-  const PORT: number = 5000;
-
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,8 +15,8 @@ const main = () => {
 
   app.use("/api", loginRoute);  
 
-  app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
+  app.listen(process.env.SYSTEM_PORT, () => {
+    console.log(`listening on port ${process.env.SYSTEM_PORT}`);
   });
 };
 
