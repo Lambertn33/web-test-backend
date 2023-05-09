738009d9a98cf6f3261cd0bf3aa7ae762975470c
Lambertn33 <lambertgithub@gmail.com>
2023-05-09 11:37:02 +0200

    feat: configure server

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express, { Express } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const main = () => {
+  const app: Express = express();
+  const PORT: number = 5000;
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cors());
+
+  app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+  });
+};
+
+main();
